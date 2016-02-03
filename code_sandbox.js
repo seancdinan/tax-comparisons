47347d3b5f7b97d42094f74edb3d823983b07307
Sean Dinan <seancdinan@gmail.com>
2016-02-03 17:09:51 -0500

    Made a simple inflation calculator

--- code_sandbox.js
+++ code_sandbox.js
@@ -1,11 +1,14 @@
+//*************************************************
+//***************** Load in the Data **************
+//*************************************************
 
 // ***** Historical US Median Incomes, 1935 to Present *****
 // Format:
 // 	"year": value,
 // 	"data": [nominalIncome, presentValueIn2015],
 // 	"source": "source info"
-var rawMedianIncomes = require('./us_historical_median_incomes.js');
-var usMedianIncomes = JSON.parse(rawMedianIncomes);
+var rawMedianIncome = require('./us_historical_median_incomes.js');
+var usMedianIncome = JSON.parse(rawMedianIncome);
 
 // ***** Median Incomes of Various Countries *****
 // Format:
@@ -13,8 +16,8 @@ var usMedianIncomes = JSON.parse(rawMedianIncomes);
 // 	"country": "Country name",
 // 	"median household income(usd)": valueIn2015,
 // 	"source": "source info"
-var rawGlobalIncomes = require('./global_median_incomes.js');
-var globalMedianIncomes = JSON.parse(rawGlobalIncomes);
+var rawGlobalIncome = require('./global_median_incomes.js');
+var globalMedianIncome = JSON.parse(rawGlobalIncome);
 
 // ***** Historical US Tax Brackets, 1935 to Present *****
 // Format:
@@ -26,8 +29,8 @@ var globalMedianIncomes = JSON.parse(rawGlobalIncomes);
 // 		"head of household": [same],
 // 	"source": "source info",
 // 	"notes": "any relevant notes"
-var rawUsTaxRates = require('./us_historical_income_tax_rates.js');
-var usTaxRates = JSON.parse(rawUsTaxRates);
+var rawUsTaxRate = require('./us_historical_income_tax_rates.js');
+var usTaxRate = JSON.parse(rawUsTaxRate);
 
 // ***** Historical US Standard Deductions and Personal Exemptions, 1935 to Present *****
 // Format:
@@ -48,15 +51,58 @@ var usTaxRates = JSON.parse(rawUsTaxRates);
 // 	- Deduction value will be $0 if none was allowed.
 // 	- Deduction value will be null if special situation applies
 // 			- See a given year's notes for specifics
-var rawUsDeductions = require('./us_historical_deductions_exemptions.js');
-var usDeductions = JSON.parse(rawUsDeductions);
+var rawUsDeduction = require('./us_historical_deductions_exemptions.js');
+var usDeduction = JSON.parse(rawUsDeduction);
 
-
-function getYear(data, year) {
-	// Get a list of years
-	var holder = [];
-	for (var i = 0; i < data.length; i++) {
-		if (data[i]["year"] = 1990)
-			return data[i]["rates"]["single"];
+//*************************************************
+//************ Helper Functions *******************
+//*************************************************
+	
+function yearAdjuster(value, year1, year2) {
+	// value in year1 equals ??? in year2
+	// value = amount in USD.
+	// year1 = Year of current value.
+	// year2 = Target year of output value.
+	if ((year1 >= 1935) && (year1 <= 2015) && (year2 >= 1935) && (year2 <= 2015) && (year1 % 5 == 0) && (year2 % 5 == 0)) {
+		for (var i = 0; i < usMedianIncome.length; i++) {
+			if (usMedianIncome[i]["year"] == year1) {
+				var medianThen1 = usMedianIncome[i]["data"][0]; 
+				var medianNow1 = usMedianIncome[i]["data"][1];
+			};
+			if (usMedianIncome[i]["year"] == year2) {
+				var medianThen2 = usMedianIncome[i]["data"][0]; 
+				var medianNow2 = usMedianIncome[i]["data"][1];
+			};
+		}
+		return value * (medianNow1 / medianThen1) * (medianThen2 / medianNow2);
 	}
+	else
+		console.log('ERROR: Please enter valid years.')
 }
+
+console.log(yearAdjuster(2000, 1935, 2000));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
