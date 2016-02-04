4d4e4d4901858309d1b3ad0a544acd8009d54107
Sean Dinan <seancdinan@gmail.com>
2016-02-04 16:28:44 -0500

    Making some cool stuff now! YearComparer lets you compare how much you would owe under two different tax plans

--- code_sandbox.js
+++ code_sandbox.js
@@ -103,31 +103,35 @@ function getDeductedValue(value, year, deductionType, exemptionType, numberOfDep
 		numberOfDependents = 0;
 
 	// Determine the amount of allowable exemptions
+	if (exemptionType == undefined)
+		exemptionType = "single person";
 	if (exemptionType == "married person")
 		exemptionCount = 2 + numberOfDependents;
-	if (exemptionType == "single person" || exemptionType == undefined)
+	else if (exemptionType == "single person")
 		exemptionCount = 1 + numberOfDependents;
+	else return 'ERROR --> getDeductedValue: Unaccepted value for exemption type';
 
-	// FOR YEARS 1970 AND LATER:
-	if (year >= 1970) {
 	// Pull the data for the given year
 	for (var i = 0; i < usDeduction.length; i++) {
 		if (usDeduction[i]["year"] == year) {
-			var deductionTotal = usDeduction[i]["deduction"][deductionType];
-			var exemptionTotal = usDeduction[i]["exemption"]["single person"] * exemptionCount;
+			if (year >= 1970 || year == 1935 || year == 1940) {
+				// FOR YEARS 1970 AND LATER, ALSO 1935/1940(THEIR DEDUCTION = 0):
+				var deductionTotal = usDeduction[i]["deduction"][deductionType];
+				var exemptionTotal = usDeduction[i]["exemption"]["single person"] * exemptionCount;
+			}
+			else if (year >= 1945 && year < 1970) {
+				// FOR YEARS 1945 THRU 1965, DEDUCTION VALUE = 10% OF INCOME UP TO $1000
+				var exemptionTotal = usDeduction[i]["exemption"][exemptionType] + (usDeduction[i]["exemption"]["amount per dependent"] * exemptionCount);
+				if ((0.1 * value) <= 1000)
+					var deductionTotal = 0.1 * value;
+				else
+					var deductionTotal = 1000;  
+			}
+			else return 'ERROR --> getDeductedValue: Something weird happened...';
 		}
 	}
-	}
-	else if (year >= 1945 && year < 1970) {
-		// THAT SPECIAL CASE
-	}
-	else if (year == 1935 || year == 1940) {
-		// THAT OTHER SPECIAL CASE
-	}
-	else
-		return 'ERROR --> getDeductedValue: Something weird happened...'
 
-	// Return the original value less the sum of exemptions and deduction.
+	// Return the original value less the sum of exemptions and deduction (or 0 if you'll owe nothing).
 	var result = value - (deductionTotal + exemptionTotal);
 	if (result < 0)
 		return 0;
@@ -140,43 +144,101 @@ function getDeductedValue(value, year, deductionType, exemptionType, numberOfDep
 }
 
 
-
 //*************************************************
 //************* Pay the Taxes *********************
 //*************************************************
 
-function taxesDue(value, year1, year2, deductionType, exemptionType, numberOfDependents) {
+function taxesDue(value, year1, year2, filingType, deductionType, exemptionType, numberOfDependents) {
 	// value in year1 owes ??? in year2
 	// Output: "You would owe $$$ in Federal Income Tax in the year YEAR2, which would be about $$$ in YEAR1"
-	// Value is any positive integer in USD
+	// Value is any positive integer in USD.
 	// year1 is any year between 1935 and 2015, falling every five years.
-	// year2 is an optional value. Default value is set to match year1.
+	// OPTIONAL: year2 default value is set to match year1. Other options are same as year1 options.
+	// OPTIONAL: filingType default value is 'single'. Other options: 'married filing jointly/separately', 'head of household'
+	// OPTIONAL: deductionType can be "single", "head of household", or "married couple". Default value is "single".
+	// OPTIONAL: exemptionType can be "married person" or "single person". Default value is "single person".
+	// OPTIONAL: Number of dependents can be any positive integer value. Default is 0.
 
 	// Let's make sure everything is OK
 	if ((year1 >= 1935) && (year1 <= 2015) && (year2 >= 1935) && (year2 <= 2015) && (year1 % 5 == 0) && (year2 % 5 == 0)) {
-	if (value < 0)
-		return 'ERROR --> getDeductedValue: I know you\'re broke, but you gotta enter at least $0';
-	if (deductionType != 'single' && deductionType != 'head of household' && deductionType != "married couple" && deductionType != undefined)
-		return 'ERROR --> getDeductedValue: Deduction Type can only be \'single\', \'head of household\' or \'married couple\'.'
-	if (exemptionType != 'married person' && exemptionType != 'single person' && exemptionType != undefined)
-		return 'ERROR --> getDeductedValue: Exemption type can only be \'married person\' or \'single person\'.'
+		if (value < 0)
+			return 'ERROR --> getDeductedValue: I know you\'re broke, but you gotta enter at least $0';
+		if (deductionType != 'single' && deductionType != 'head of household' && deductionType != "married couple" && deductionType != undefined)
+			return 'ERROR --> getDeductedValue: Deduction Type can only be \'single\', \'head of household\' or \'married couple\'.'
+		if (exemptionType != 'married person' && exemptionType != 'single person' && exemptionType != undefined)
+			return 'ERROR --> getDeductedValue: Exemption type can only be \'married person\' or \'single person\'.'
+		// Give some default values to optional arguments
+		if (year2 == undefined)
+			year2 = year1;
+		if (filingType == undefined)
+			filingType = 'single';
+		if (deductionType == undefined)
+			deductionType = 'single';
+		if (exemptionType == undefined)
+			exemptionType = 'single person';
+		if (numberOfDependents == undefined)
+			numberOfDependents = 0;
 	
-	// CODE GOES HERE
+		// Find out the taxable amount
+		var adjustedValue = yearAdjuster(value, year1, year2);
+		var taxableAmount = getDeductedValue(adjustedValue, year2, deductionType, exemptionType, numberOfDependents);
+		
+		// Get tax brackets
+		for (var i = 0; i < usTaxRate.length; i++) {
+			if (usTaxRate[i]["year"] == year2) {
+				var brackets = usTaxRate[i]["rates"][filingType];
+			}
+		}
 
+		// Go thru the brackets to find total amount due
+		var runningTotal = 0;
+		var done = false;
+		// This will spit out the total amount due in year2 dollars
+		for (var i = 0; i < brackets.length; i++){
+			if (done == false) {
+				if (taxableAmount >= brackets[i][2] && brackets[i][2] != null) {
+					runningTotal += brackets[i][0] * (brackets[i][2] - brackets[i][1]);
+				}
+				else if (taxableAmount < brackets[i][2] || brackets[i][2] == null) {
+					runningTotal += brackets[i][0] * (taxableAmount - brackets[i][1]);
+					done = true;
+				}
+				else return 'ERROR --> taxesDue: Something weird happened...'
+			}
+		}
 
+		var year1Total = yearAdjuster(runningTotal, year2, year1);
+		// if (year1 != year2) {
+		// 	return ['\nIf you made $', value, ' in ', year1, ',\nYou would owe $', year1Total.toFixed(2),
+		// 					' with the ', year2, ' tax plan.\nThat would be about $', runningTotal.toFixed(2), ' in ', year2, '.\n'].join('');}
+		// else return ['\nYou would owe $', runningTotal.toFixed(2), ' on $', value.toFixed(2), ' in Federal Income Tax in ', year1, '\n'].join('');
+		return year1Total;
 	}
 	else
 		return 'ERROR --> taxesDue: Please enter valid years';
 }
 
+//*************************************************
+//************  Compare 2 Years   *****************
+//*************************************************
+
+function yearComparer(value, year1, year2, filingType, deductionType, exemptionType, numberOfDependents) {
+	var year1Value = taxesDue(value,year1,year1,filingType,deductionType,exemptionType,numberOfDependents);
+	var year2Value = yearAdjuster(taxesDue(yearAdjuster(value,year1,year2),year2,year2,filingType,deductionType,exemptionType,numberOfDependents),year2,year1);
+	var difference = year1Value - year2Value;
+	var diffPercent= difference/year1Value;
+	//return [year1Value.toFixed(2), year2Value.toFixed(2)];
+	return ['\nIf you made $', value, ' in ', year1, ',\nYou would owe $', year1Value.toFixed(2),
+					 ' under a ', year1, ' tax plan.\nYou would owe $', year2Value.toFixed(2), ' under a ', year2, ' tax plan.',
+					 '\nThat\'s a difference of $', difference.toFixed(2), '! (', (diffPercent*100).toFixed(2), '%)\n'].join('')
+}
+
+
 
 //*************************************************
 //*************  Run Some Tests!  *****************
 //*************************************************
-	
-console.log(getDeductedValue(20000, 1945, "single", "single person", 4));
-
-
+console.log(yearComparer(200000000, 2015, 1940))
 
 
 
