b859b87a1c7f3bd907212ca76c98112e770d0e6b
Sean Dinan <seancdinan@gmail.com>
2016-02-20 13:51:43 -0500

    Tax functions done for 18 countries now (US included). Going to pause on data collection for now to start on the site development. Will continue adding countries once mvp is running

--- globalRates.js
+++ globalRates.js
@@ -25,6 +25,7 @@ function template(income,status){
 	return [totalTax, totalTax/income]
 }
 
+// EUROPE
 function germany(income,status){
 	var sources = [['General Info', 'https://en.wikipedia.org/wiki/Taxation_in_Germany'],['VAT tax','http://www.tradingeconomics.com/germany/sales-tax-rate']];
 	var moneyLeft = income;
@@ -375,5 +376,431 @@ function belgium(income,status, dependents){
 	totalTax += socSec;
 	return [totalTax, totalTax/income]
 }
+// UP THRU FEB 19
+function greece(income,status){
+	// Currency: Euro
+	// VAT CURRENTLY UNIMPLEMENTED
+	var sources = [['General Info','https://en.wikipedia.org/wiki/Taxation_in_Greece'],['VAT Breakdown','http://livingingreece.gr/2010/07/01/vat-fpa-taxes-greece/']];
+	// Default Values
+	if (status == 'undefined'){status == 'single';}
 
-console.log(belgium(20000))
+	// Deductions
+	var deductions = 0;
+
+	var netIncome = income - deductions;
+	var totalTax = 0;
+
+	// Income Tax
+	if (netIncome < 25000){totalTax += 0.22 * netIncome;}
+	if (netIncome >= 25000){totalTax += 0.22 * (25000 - 0);}
+
+	if (netIncome >= 42000){totalTax += 0.32 * (42000 - 25000);}
+	if (netIncome < 42000 && netIncome > 25000){totalTax += 0.32 * (netIncome - 25000);}
+
+	if (netIncome > 42000){totalTax += 0.42 * (netIncome - 42000);}
+
+	// VAT Tax [general, groceries, books and a few other things]
+	var vat = [0.23, 0.13, 0.065];
+	var vatSum = 0;
+
+	// Social Security
+	var socSec = 0.155 * income;
+
+	totalTax += socSec;
+	return [totalTax, totalTax/income]
+}
+
+function czechRepublic(income,status){
+	// Currency: Czech Koruna
+	// VAT CURRENTLY UNIMPLEMENTED
+	var sources = [['General Info','https://en.wikipedia.org/wiki/Taxation_in_the_Czech_Republic']];
+	// Default Values
+	if (status == 'undefined'){status == 'single';}
+
+	// Deductions
+	var deductions = 0;
+
+	var netIncome = income - deductions;
+	var totalTax = 0;
+
+	// Income Tax
+	totalTax += 0.15 * netIncome;
+
+	// VAT Tax [general, groceries, misc]
+	var vat = [0.21, 0.15];
+	var vatSum = 0;
+
+	// Social Security
+	var socSec += 0.13 * income;
+
+	totalTax += socSec;
+	return [totalTax, totalTax/income]
+}
+
+function portugal(income,status,dependents){
+	// Currency: Euro
+	// VAT CURRENTLY UNIMPLEMENTED
+	var sources = [['General Info','https://en.wikipedia.org/wiki/Taxation_in_Portugal'],['Deductions','https://www.justlanded.com/english/Portugal/Portugal-Guide/Money/Income-tax-allowances']];
+	// Default Values
+	if (status == 'undefined'){status == 'single';}
+	if (dependents == 'undefined'){dependents = 0;}
+
+	// Deductions
+	var deductions = 0;
+	deductions += 0.11 * income;
+	deductions += 175.88;
+	var baseDeduction = 0.70 * income;
+	if (baseDeduction > 2484){baseDeduction = 2484;}
+	deductions += baseDeduction;
+	if (dependents > 0){
+		for (var i = 1; i <= dependents; i++){
+			deductions += 96.77;
+		}
+	}
+	if (status == 'married'){deductions += 133.68;}
+
+	var netIncome = income - deductions;
+	var totalTax = 0;
+
+	// Income Tax
+	if (netIncome >= 7000){totalTax += 0.145 * (7000 - 0);}
+	if (netIncome < 7000){totalTax += 0.145 * netIncome;}
+
+	if (netIncome >= 20000){totalTax += 0.285 * (20000 - 7000);}
+	if (netIncome < 20000 && netIncome > 7000){totalTax += 0.285 * (netIncome - 7000);}
+
+	if (netIncome >= 40000){totalTax += 0.37 * (40000 - 20000);}
+	if (netIncome < 40000 && netIncome > 20000){totalTax += 0.37 * (netIncome - 20000);}
+
+	if (netIncome >= 80000){totalTax += 0.45 * (80000 - 40000);}
+	if (netIncome < 80000 && netIncome > 40000){totalTax += 0.45 * (netIncome - 40000);}
+
+	if (netIncome > 80000){totalTax += 0.48 * (netIncome - 80000);}
+
+	// VAT Tax [general, groceries, wine/water/cultural events]
+	var vat = [0.23, 0.06, 0.12];
+	var vatSum = 0;
+
+	// Social Security
+	var socSec = 0.11 * income;
+
+	totalTax += socSec;
+	return [totalTax, totalTax/income]
+}
+
+function hungary(income,status, dependents){
+	// Currency: HUF
+	// VAT CURRENTLY UNIMPLEMENTED
+	var sources = [['General Info','https://en.wikipedia.org/wiki/Taxation_in_Hungary'],['Social Security','https://hro.ceu.edu/social-security-system']];
+	// Default Values
+	if (status == 'undefined'){status == 'single';}
+	if (dependents == 'undefined'){dependents = 0;}
+
+	// Deductions
+	var deductions = 0;
+	if (dependents > 0 && dependents < 3){
+		for (var i = 1; i <= dependents; i++){deductions += 62500;}
+	}
+	else if (dependents > 2){
+		for (var i = 1; i <= dependents; i++){deductions += 206250;}
+	}
+	var netIncome = income - deductions;
+	var totalTax = 0;
+
+	// Income Tax
+	totalTax += 0.16 * netIncome;
+
+	// VAT Tax [general, bakery/dairy, medicine]
+	var vat = [0.27, 0.18, 0.05];
+	var vatSum = 0;
+
+	// Social Security
+	var socSec = 0.185 * income;
+
+	totalTax += socSec;
+	return [totalTax, totalTax/income]
+}
+
+function sweden(income,status){
+	// Currency: kroner
+	// VAT CURRENTLY UNIMPLEMENTED
+	var sources = [['General Info','https://en.wikipedia.org/wiki/Taxation_in_Sweden'],['Deductions','https://home.kpmg.com/xx/en/home/insights/2011/12/sweden-income-tax.html'],['Social Security','https://www.ssa.gov/policy/docs/progdesc/ssptw/2014-2015/europe/sweden.html']];
+	// Default Values
+	if (status == 'undefined'){status == 'single';}
+
+	// Deductions
+	var deductions = 0;
+	if (income <= 44400){deductions += 18900;}
+	if (income > 44400 && income <= 120900){deductions += ((income - 44400)/(120900 - 44400)) * (34200 - 19000);}
+	if (income > 120900 && income <= 139000){deductions += 34300;}
+	if (income > 139000 && income <= 350000){deductions += ((income - 139000)/(350000 - 139000)) * (34200 - 13200);}
+	if (income > 350000){deductions += 13100;}
+
+	var netIncome = income - deductions;
+	if (netIncome < 0){netIncome = 0;}
+	var totalTax = 0;
+
+	// Income Tax
+	if (netIncome <= 18800){totalTax += 0;}
+
+	if (netIncome >= 433900){totalTax += 0.31 * (433900 - 18800);}
+	if (netIncome > 18800 && netIncome < 433900){totalTax += 0.31 * (netIncome - 18800);}
+
+	if (netIncome >= 615700){totalTax += 0.51 * (615700 - 433900);}
+	if (netIncome > 433900 && netIncome < 615700){totalTax += 0.51 * (netIncome - 433900);}
+
+	if (netIncome > 615700){totalTax += 0.56 * (netIncome - 615700);}
+
+	// VAT Tax [general, groceries, cultural events]
+	var vat = [0.25, 0.12, 0.06];
+	var vatSum = 0;
+
+	// Social Security
+	var socSec = 0;
+	if (netIncome >= 18781 && netIncome <= 459183){socSec += 0.07451 * netIncome;}
+	if (netIncome > 459183){socSec += 0.07451 * 459183;}
+
+	return [totalTax, totalTax/income]
+}
+
+function austria(income,status){
+	// Currency: Euro
+	// VAT CURRENTLY UNIMPLEMENTED
+	var sources = [['General Info','https://en.wikipedia.org/wiki/Taxation_in_Austria'],['Social Security','https://www.ssa.gov/policy/docs/progdesc/ssptw/2012-2013/europe/austria.html']];
+	// Default Values
+	if (status == 'undefined'){status == 'single';}
+
+	// Deductions
+	var deductions = 0;
+
+	var netIncome = income - deductions;
+	var totalTax = 0;
+
+	// Income Tax
+
+	// VAT Tax [general, groceries, misc]
+	var vat = [];
+	var vatSum = 0;
+
+	// ### OTHER TAXES ###
+
+	return [totalTax, totalTax/income]
+}
+
+function switzerland(income,status){
+	// Currency: #####
+	// VAT CURRENTLY UNIMPLEMENTED
+	var sources = [['VAT tax','https://en.wikipedia.org/wiki/Taxation_in_Switzerland'],['Social Security','https://www.ssa.gov/policy/docs/progdesc/ssptw/2012-2013/europe/switzerland.html'],['Income Tax','http://taxsummaries.pwc.com/uk/taxsummaries/wwts.nsf/ID/Switzerland-Individual-Taxes-on-personal-income']];
+	// Default Values
+	if (status == 'undefined'){status == 'single';}
+
+	// Deductions
+	var deductions = 0;
+
+	var netIncome = income - deductions;
+	var totalTax = 0;
+
+	// Income Tax
+
+	// VAT Tax [general, groceries, misc]
+	var vat = [];
+	var vatSum = 0;
+
+	// ### OTHER TAXES ###
+
+	return [totalTax, totalTax/income]
+}
+
+function bulgaria(income,status){
+	// Currency: #####
+	// VAT CURRENTLY UNIMPLEMENTED
+	var sources = [['General Info','']];
+	// Default Values
+	if (status == 'undefined'){status == 'single';}
+
+	// Deductions
+	var deductions = 0;
+
+	var netIncome = income - deductions;
+	var totalTax = 0;
+
+	// Income Tax
+
+	// VAT Tax [general, groceries, misc]
+	var vat = [];
+	var vatSum = 0;
+
+	// ### OTHER TAXES ###
+
+	return [totalTax, totalTax/income]
+}
+
+function serbia(income,status){
+	// Currency: #####
+	// VAT CURRENTLY UNIMPLEMENTED
+	var sources = [['General Info','']];
+	// Default Values
+	if (status == 'undefined'){status == 'single';}
+
+	// Deductions
+	var deductions = 0;
+
+	var netIncome = income - deductions;
+	var totalTax = 0;
+
+	// Income Tax
+
+	// VAT Tax [general, groceries, misc]
+	var vat = [];
+	var vatSum = 0;
+
+	// ### OTHER TAXES ###
+
+	return [totalTax, totalTax/income]
+}
+
+function denmark(income,status){
+	// Currency: #####
+	// VAT CURRENTLY UNIMPLEMENTED
+	var sources = [['General Info','']];
+	// Default Values
+	if (status == 'undefined'){status == 'single';}
+
+	// Deductions
+	var deductions = 0;
+
+	var netIncome = income - deductions;
+	var totalTax = 0;
+
+	// Income Tax
+
+	// VAT Tax [general, groceries, misc]
+	var vat = [];
+	var vatSum = 0;
+
+	// ### OTHER TAXES ###
+
+	return [totalTax, totalTax/income]
+}
+
+function finland(income,status){
+	// Currency: #####
+	// VAT CURRENTLY UNIMPLEMENTED
+	var sources = [['General Info','']];
+	// Default Values
+	if (status == 'undefined'){status == 'single';}
+
+	// Deductions
+	var deductions = 0;
+
+	var netIncome = income - deductions;
+	var totalTax = 0;
+
+	// Income Tax
+
+	// VAT Tax [general, groceries, misc]
+	var vat = [];
+	var vatSum = 0;
+
+	// ### OTHER TAXES ###
+
+	return [totalTax, totalTax/income]
+}
+
+function norway(income,status){
+	// Currency: #####
+	// VAT CURRENTLY UNIMPLEMENTED
+	var sources = [['General Info','']];
+	// Default Values
+	if (status == 'undefined'){status == 'single';}
+
+	// Deductions
+	var deductions = 0;
+
+	var netIncome = income - deductions;
+	var totalTax = 0;
+
+	// Income Tax
+
+	// VAT Tax [general, groceries, misc]
+	var vat = [];
+	var vatSum = 0;
+
+	// ### OTHER TAXES ###
+
+	return [totalTax, totalTax/income]
+}
+
+function ireland(income,status){
+	// Currency: #####
+	// VAT CURRENTLY UNIMPLEMENTED
+	var sources = [['General Info','']];
+	// Default Values
+	if (status == 'undefined'){status == 'single';}
+
+	// Deductions
+	var deductions = 0;
+
+	var netIncome = income - deductions;
+	var totalTax = 0;
+
+	// Income Tax
+
+	// VAT Tax [general, groceries, misc]
+	var vat = [];
+	var vatSum = 0;
+
+	// ### OTHER TAXES ###
+
+	return [totalTax, totalTax/income]
+}
+
+function croatia(income,status){
+	// Currency: #####
+	// VAT CURRENTLY UNIMPLEMENTED
+	var sources = [['General Info','']];
+	// Default Values
+	if (status == 'undefined'){status == 'single';}
+
+	// Deductions
+	var deductions = 0;
+
+	var netIncome = income - deductions;
+	var totalTax = 0;
+
+	// Income Tax
+
+	// VAT Tax [general, groceries, misc]
+	var vat = [];
+	var vatSum = 0;
+
+	// ### OTHER TAXES ###
+
+	return [totalTax, totalTax/income]
+}
+
+function iceland(income,status){
+	// Currency: #####
+	// VAT CURRENTLY UNIMPLEMENTED
+	var sources = [['General Info','']];
+	// Default Values
+	if (status == 'undefined'){status == 'single';}
+
+	// Deductions
+	var deductions = 0;
+
+	var netIncome = income - deductions;
+	var totalTax = 0;
+
+	// Income Tax
+
+	// VAT Tax [general, groceries, misc]
+	var vat = [];
+	var vatSum = 0;
+
+	// ### OTHER TAXES ###
+
+	return [totalTax, totalTax/income]
+}
+
+console.log(belgium(72000))
